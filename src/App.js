44d2b5a17cd6f33e6ceafe5151acf733d1c5e7ce
shataniya <47215073+shataniya@users.noreply.github.com>
2020-05-18 17:24:35 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+    render(){
+        return (
+            <div className='app'>
+                <h1>Hello, World!</h1>
+            </div>
+        )
+    }
+}
+
+export default App
