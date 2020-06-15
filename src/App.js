5bb857cdd12d0fe2e83179590157ac9702d04c94
shataniya <47215073+shataniya@users.noreply.github.com>
2020-06-15 17:46:10 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import style from './App.css'
 
 class App extends React.Component {
     constructor(props){
@@ -8,7 +8,7 @@ class App extends React.Component {
     }
     render(){
         return (
-            <div className='app'>
+            <div className={style.app}>
                 <h1>Hello, World!</h1>
             </div>
         )
